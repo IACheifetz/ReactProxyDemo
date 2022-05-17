c0465c829cd962f3bf06d15349a8b981c8a35a74
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-05-16 17:07:32 -0700

    henceforth he shall walk

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,14 +1,14 @@
 
 
 export async function getPokemon(searchFilter) {
-  const rawResponse = await fetch(`/.netlify/functions.cat-endpoint?pokemon=${searchFilter}`);
+  const rawResponse = await fetch(`/.netlify/functions.pokeyman?pokemon=${searchFilter}`);
   const data = await rawResponse.json();
 
   return data;
 }
 
 export async function getPokemonByName() {
-  const rawResponse = await fetch('http://localhost:8888/.netlify/functions/cat-endpoint');
+  const rawResponse = await fetch('http://localhost:8888/.netlify/functions/pokeyman');
   const data = await rawResponse.json();
   
   return data;
