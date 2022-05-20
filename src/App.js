c0af5da9762b693b563fe99e53b7b2660c225058
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-05-20 11:35:08 -0700

    added loading spinner

--- src/App.js
+++ src/App.js
@@ -1,52 +1,19 @@
-//import logo from './logo.svg';
+
 import './App.css';
-import { useEffect, useState } from 'react';
-import { getPokemon } from './services/fetch-utils';
+import PokemonItem from './pokemonItem';
+// import LoadingSpinner from './LoadingSpinner';
+// import { useState } from 'react';
 
 function App() {
-  const [pokemon, setPokemon] = useState([]);
-  const [search, setSearch] = useState('char');
-
-  // async function load() {
-  //   //console.log(search); //eslint-disable-line
-  //   const data = await getPokemon(search);
-
-    
-  //   //console.log(data, 2); //eslint-disable-line
-  //   return data;
-  // }
-
-  useEffect(() => {
-    const fetchPokemon = async () => {
-      const data = await getPokemon(search);
+  // const [isLoading] = useState(true);
   
-      if (data) {
-        setPokemon(data);
-      }
-    };
-  
-    fetchPokemon();
-  }, [search]);
-
-  // function handleSearch(e) {
-  //   e.preventDefault();
-
-  
-  // }
 
 
-  //console.log(pokemon); //eslint-disable-line
   return (
     <div className="App">
-      <form >
-        <input value={search} onChange={e => setSearch(e.target.value)}/>
-      </form>
-      <header className="App-header">
-        {pokemon.map((p, i) => <div key={i}>
-          <h2>{p.pokemon}</h2>
-          <p>weight: {p.weight}</p>
-        </div>)}
-      </header>
+      {/* {isLoading === true ?
+        <LoadingSpinner/> : */}
+      <PokemonItem />
     </div>
   );
 }
